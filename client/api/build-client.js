96594d3d1986ff2cf8ec70714c0838fc6112927a
saradchhetri07 <saradchhetri20690@gmail.com>
2024-10-29 14:30:04 +0545

    added build-client for code reusability

---
+++ client/api/build-client.js
@@ -0,0 +1,18 @@
+import axios from "axios";
+
+export default ({ req }) => {
+  if (typeof window === "undefined") {
+    // we are on the server
+    // requests should be made with base url of http://ingress-nginx.ingress-nginx.srv.local
+    // http://SERVICENAME.NAMESPACE.svc.cluster.local
+    return axios.create({
+      baseURL:
+        "http://ingress-nginx-controller.ingress-nginx.srv.cluster.local",
+      headers: req.headers,
+    });
+  } else {
+    return axios.create({
+      baseURL: "/",
+    });
+  }
+};
--- client/pages/index.js
+++ client/pages/index.js
@@ -1,7 +1,15 @@
-export default function Home() {
+import buildClient from "../api/build-client";
+
+const landingPage = ({ currentUser }) => {
   return (
     <div>
       <h1>Hello</h1>
     </div>
   );
-}
+};
+
+landingPage.getInitialProps = async (context) => {
+  const { data } = buildClient(context).get("/api/users/currentUser");
+  return data;
+};
+export default landingPage;
